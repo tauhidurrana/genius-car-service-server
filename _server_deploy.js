f8e9273f111260e80d17aa4dab2e6bff9ca975dc
tauhidurrana <tauhidurrahman.rana@gmail.com>
2022-05-07 12:31:24 +0600

    mongo api added and jwt token and steps of heroku added

---
+++ _server_deploy.js
@@ -0,0 +1,13 @@
+/**
+ * one time for your computer
+ * ---------------------
+ * 1. create heroku account
+ * 2. verify email
+ * 3. install heroku cli
+ * 4. type heroku login on command 
+ * 5. heroku login
+ * --------------------
+ * For each project one time
+ * 1. heroku create
+ * 2. make sure your git add . git commit and git push updated
+ */
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require ('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -18,7 +19,18 @@ async function run (){
     try{
         await client.connect()
         const serviceCollection = client.db('geniusCar').collection('service');
+        const orderCollection = client.db('geniusCar').collection('order');
 
+        // Auth
+        app.post('/login', async(req, res)=>{
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{
+                expiresIn:'1d'
+            })
+            res.send(accessToken);
+        })
+
+        // Services API
         app.get('/service', async(req, res)=>{
             const query = {};
             const cursor = serviceCollection.find(query);
@@ -39,6 +51,27 @@ async function run (){
             const result = await serviceCollection.insertOne(newService);
             res.send(result);
         })
+
+        // Delete service
+
+        // Order collection API
+
+        app.get('/order', async(req, res)=>{
+            const email = req.query.email;
+            console.log(email);
+            const query = {email:email};
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        })
+
+        app.post('/order', async(req, res)=>{
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        })
+
+        
     }
     finally{
 
